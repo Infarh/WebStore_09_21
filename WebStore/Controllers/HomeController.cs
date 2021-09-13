8f7aed9cd727e8954634de91cbf61e4517bed354
Infarh <shmachilin@yandex.ru>
2021-09-13 20:54:04 +0300

    Представление сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 31 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+        };
+
         public IActionResult Index() // http://localhost:5000/Home/Index
         {
             //return Content("Hello from first controller");
@@ -13,6 +22,11 @@ namespace WebStore.Controllers
         public IActionResult SeconAction(int id)
         {
             return Content($"Second action with parameter {id}");
+        }
+
+        public IActionResult Employees() // http://localhost:5000/Home/Employees
+        {
+            return View(__Employees);
         }
     }
 }
