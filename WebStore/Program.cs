74062fb0a2f089dd1a62f424fd4432df761f7d86
Infarh <shmachilin@yandex.ru>
2021-09-13 20:15:29 +0300

    Чистка кода от хлама

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace WebStore
 {
@@ -14,14 +7,16 @@ namespace WebStore
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host_builder = CreateHostBuilder(args);
+            var host = host_builder.Build();
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(host => host
+                   .UseStartup<Startup>()
+                )
+            ;
     }
 }
