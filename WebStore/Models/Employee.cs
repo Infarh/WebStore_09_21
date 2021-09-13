2da5a1e332cf7b50a90254a7042e0fb55b93d682
Infarh <shmachilin@yandex.ru>
2021-09-13 20:53:54 +0300

    Модель сотрудника

---
+++ WebStore/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace WebStore.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public int Age { get; set; }
+    }
+
+    //public record Employee2(int Id, string LastName, string FirstName, string Patronymic, int Age);
+}
