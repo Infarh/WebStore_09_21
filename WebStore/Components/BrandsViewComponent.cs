637e9adda740fd4494d4e9474783bb3323c954f4
Infarh <shmachilin@yandex.ru>
2021-09-23 20:50:27 +0300

    Заготовки визуальных компонент

---
+++ WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components
+{
+    //[ViewComponent(Name = "BrandsView")]
+    public class BrandsViewComponent : ViewComponent
+    {
+        //public async Task<IViewComponentResult> InvokeAsync() => View();
+
+        public IViewComponentResult Invoke() => View();
+    }
+}
---
+++ WebStore/Components/SectionsViewComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components
+{
+    public class SectionsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
