ce6b58d94a332dfb935a440158865167c5f0d43a
Infarh <shmachilin@yandex.ru>
2021-09-23 20:40:35 +0300

    Сервис каталога товаров + набор тестовых данных

---
+++ WebStore/Services/Interfaces/IProductData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Services.Interfaces
+{
+    public interface IProductData
+    {
+        IEnumerable<Section> GetSections();
+
+        IEnumerable<Brand> GetBrands();
+    }
+}
