d945c6c1779c216146aef211df7e9e55f14be824
Infarh <shmachilin@yandex.ru>
2021-09-23 21:10:45 +0300

    Определение товара

---
+++ WebStore.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities
+{
+    public class Product : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+
+        public int SectionId { get; set; }
+
+        public int? BrandId { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
