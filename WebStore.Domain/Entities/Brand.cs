b1508e3cae8fd11507ccceecd8896ef6e5c871fa
Infarh <shmachilin@yandex.ru>
2021-09-27 20:15:26 +0300

    Доработка набора сущностей для использования их в EF

--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -1,10 +1,15 @@
-﻿using WebStore.Domain.Entities.Base;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities
 {
+    [Index(nameof(Name)/*, nameof(Order)*/, IsUnique = true)]
+    //[Table("Brandsss")]
     public class Brand : NamedEntity, IOrderedEntity
     {
+        //[Column("BrandOrder")]
         public int Order { get; set; }
     }
 }
