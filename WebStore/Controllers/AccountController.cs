c41c904dceb02487b17816dd8373918c06b25b00
Infarh <shmachilin@yandex.ru>
2021-09-30 20:18:03 +0300

    Добавление Менеджеров Identity

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
 
 namespace WebStore.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> _UserManager;
+        private readonly SignInManager<User> _SignInManager;
+
+        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
+        {
+            _UserManager = UserManager;
+            _SignInManager = SignInManager;
+        }
+
         public IActionResult Register() => View();
 
         public IActionResult Login() => View();
