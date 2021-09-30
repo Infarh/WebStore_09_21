9515014d0eb4a661dec0cc6390e818e4fe774524
Infarh <shmachilin@yandex.ru>
2021-09-30 19:39:37 +0300

    Модели пользователя и роли

---
+++ WebStore.Domain/Entities/Identity/Role.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebStore.Domain.Entities.Identity
+{
+    public class Role : IdentityRole
+    {
+        public const string Administrators = "Administrators";
+
+        public const string Users = "Users";
+    }
+}
---
+++ WebStore.Domain/Entities/Identity/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebStore.Domain.Entities.Identity
+{
+    public class User : IdentityUser
+    {
+        public const string Administrator = "Admin";
+
+        public const string DefaultAdminPassword = "AdPAss_123";
+    }
+}
