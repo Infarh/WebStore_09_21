d0fa9da2603e45de8c138d5ccc0293451c615289
Infarh <shmachilin@yandex.ru>
2021-10-04 20:02:32 +0300

    Наделение всех новых пользователей ролью User

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -38,6 +38,8 @@ namespace WebStore.Controllers
             {
                 await _SignInManager.SignInAsync(user, false);
 
+                await _UserManager.AddToRoleAsync(user, Role.Users);
+
                 return RedirectToAction("Index", "Home");
             }
 
