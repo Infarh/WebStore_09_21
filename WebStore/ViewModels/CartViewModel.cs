6bf6ede01f967192034c7089419bf8d3231b107d
Infarh <shmachilin@yandex.ru>
2021-10-04 21:10:09 +0300

    Модель-представления корзины

--- WebStore/ViewModels/CartViewModel.cs
+++ WebStore/ViewModels/CartViewModel.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WebStore.ViewModels
 {
     public class CartViewModel
     {
-        
+        public IEnumerable<(ProductViewModel Product, int Quantity)> Items { get; set; }
+
+        public int ItemsCount => Items?.Sum(item => item.Quantity) ?? 0;
+
+        public decimal TotalPrice => Items?.Sum(item => item.Product.Price * item.Quantity) ?? 0m;
     }
 }
