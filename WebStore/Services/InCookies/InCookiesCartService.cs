d391d1da8818203ebc1cf9c21e9f6b80a3c6c693
Infarh <shmachilin@yandex.ru>
2021-10-04 21:07:38 +0300

    Основная логика корзины

--- WebStore/Services/InCookies/InCookiesCartService.cs
+++ WebStore/Services/InCookies/InCookiesCartService.cs
@@ -1,5 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
 using Microsoft.AspNetCore.Http;
+
+using Newtonsoft.Json;
+
+using WebStore.Domain.Entities;
 using WebStore.Services.Interfaces;
 using WebStore.ViewModels;
 
@@ -11,6 +18,33 @@ namespace WebStore.Services.InCookies
         private readonly IProductData _ProductData;
         private readonly string _CartName;
 
+        private Cart Cart
+        {
+            get
+            {
+                var context = _HttpContextAccessor.HttpContext;
+                var cookies = context!.Response.Cookies;
+
+                var cart_cookies = context.Request.Cookies[_CartName];
+                if (cart_cookies is null)
+                {
+                    var cart = new Cart();
+                    cookies.Append(_CartName, JsonConvert.SerializeObject(cart));
+                    return cart;
+                }
+
+                ReplaceCart(cookies, cart_cookies);
+                return JsonConvert.DeserializeObject<Cart>(cart_cookies);
+            }
+            set => ReplaceCart(_HttpContextAccessor.HttpContext!.Response.Cookies, JsonConvert.SerializeObject(value));
+        }
+
+        private void ReplaceCart(IResponseCookies cookies, string cart)
+        {
+            cookies.Delete(_CartName);
+            cookies.Append(_CartName, cart);
+        }
+
         public InCookiesCartService(IHttpContextAccessor HttpContextAccessor, IProductData ProductData)
         {
             _HttpContextAccessor = HttpContextAccessor;
@@ -19,17 +53,62 @@ namespace WebStore.Services.InCookies
             var user = HttpContextAccessor.HttpContext!.User;
             var user_name = user.Identity!.IsAuthenticated ? $"-{user.Identity.Name}" : null;
 
-            _CartName = $"WebStore.Cart{user_name}";
+            _CartName = $"GB.WebStore.Cart{user_name}";
         }
 
-        public void Add(int Id) { throw new NotImplementedException(); }
+        public void Add(int Id)
+        {
+            var cart = Cart;
 
-        public void Decrement(int Id) { throw new NotImplementedException(); }
+            var item = cart.Items.FirstOrDefault(i => i.ProductId == Id);
+            if (item is null)
+                cart.Items.Add(new CartItem { ProductId = Id, Quantity = 1 });
+            else
+                item.Quantity++;
 
-        public void Remove(int Id) { throw new NotImplementedException(); }
+            Cart = cart;
+        }
 
-        public void Clear() { throw new NotImplementedException(); }
+        public void Decrement(int Id)
+        {
+            var cart = Cart;
 
-        public CartViewModel GetViewModel() { throw new NotImplementedException(); }
+            var item = cart.Items.FirstOrDefault(i => i.ProductId == Id);
+            if (item is null) return;
+
+            if (item.Quantity > 0)
+                item.Quantity--;
+
+            if (item.Quantity <= 0)
+                cart.Items.Remove(item);
+
+            Cart = cart;
+        }
+
+        public void Remove(int Id)
+        {
+            var cart = Cart;
+
+            var item = cart.Items.FirstOrDefault(i => i.ProductId == Id);
+            if (item is null) return;
+
+            cart.Items.Remove(item);
+
+            Cart = cart;
+        }
+
+        public void Clear()
+        {
+            //Cart = new();
+
+            var cart = Cart;
+            cart.Items.Clear();
+            Cart = cart;
+        }
+
+        public CartViewModel GetViewModel()
+        {
+            return null;
+        }
     }
 }
