d9d15a26a7852bd0cadeb01c4f641529412bb6da
Infarh <shmachilin@yandex.ru>
2021-10-07 19:30:45 +0300

    Заготовка контроллера управления списком товаров

---
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
