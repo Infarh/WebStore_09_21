e7ce0bedf10456575a50cd59de09d2074528d669
Infarh <shmachilin@yandex.ru>
2021-10-07 20:00:09 +0300

    Добавление заказа в контекст БД

--- WebStore.Domain/Entities/Orders/Order.cs
+++ WebStore.Domain/Entities/Orders/Order.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Identity;
 
@@ -25,6 +26,9 @@ namespace WebStore.Domain.Entities.Orders
         public DateTimeOffset Date { get; set; } = DateTimeOffset.UtcNow;
 
         public ICollection<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        [NotMapped]
+        public decimal TotalPrice => Items?.Sum(i => i.TotalItemPrice) ?? 0m;
     }
 
     public class OrderItem : Entity
