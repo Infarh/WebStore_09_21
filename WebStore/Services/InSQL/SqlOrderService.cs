c84b055567cca64ed5484291da1f341fb1569bd3
Infarh <shmachilin@yandex.ru>
2021-10-07 20:08:43 +0300

    Заготовка реализации сервиса заказов

---
+++ WebStore/Services/InSQL/SqlOrderService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Domain.Entities.Orders;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Services.InSQL
+{
+    public class SqlOrderService : IOrderService
+    {
+        private readonly WebStoreDB _db;
+        private readonly UserManager<User> _UserManager;
+
+        public SqlOrderService(WebStoreDB db, UserManager<User> UserManager)
+        {
+            _db = db;
+            _UserManager = UserManager;
+        }
+
+        public async Task<IEnumerable<Order>> GetUserOrders(string User) { throw new System.NotImplementedException(); }
+
+        public async Task<Order> GetOrderById(int id) { throw new System.NotImplementedException(); }
+
+        public async Task<Order> CreateOrder(string UserName, CartViewModel Cart, OrderViewModel OrderModel) { throw new System.NotImplementedException(); }
+    }
+}
