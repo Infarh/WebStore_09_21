da15075af5705cd621a113dc8ae81b28c079c140
Infarh <shmachilin@yandex.ru>
2021-10-11 21:15:56 +0300

    Визуализация данных, полученных от WebAPI

---
+++ UI/WebStore/Controllers/WebAPIController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.TestAPI;
+
+namespace WebStore.Controllers
+{
+    public class WebAPIController : Controller
+    {
+        private readonly IValuesService _ValuesService;
+
+        public WebAPIController(IValuesService ValuesService) => _ValuesService = ValuesService;
+
+        public IActionResult Index()
+        {
+            var values = _ValuesService.GetAll();
+            return View(values);
+        }
+    }
+}
