be7562cfd266850a31e7445556400b97ff845e53
Infarh <shmachilin@yandex.ru>
2021-10-14 21:14:38 +0300

    Заготовка контроллера WebAPI сервиса заказов

---
+++ Services/WebStore.WebAPI/Controllers/OrdersApiController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/orders")]
+    public class OrdersApiController : ControllerBase
+    {
+        private readonly IOrderService _OrderService;
+
+        public OrdersApiController(IOrderService OrderService) => _OrderService = OrderService;
+
+        [HttpGet("user/{UserName}")]
+        public async Task<IActionResult> GetUserOrders(string UserName)
+        {
+            var orders = await _OrderService.GetUserOrders(UserName);
+            return Ok(orders);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderById(int id)
+        {
+            var order = await _OrderService.GetOrderById(id);
+            if (order is null)
+                return NotFound();
+            return Ok(order);
+        }
+
+        [HttpPost("{UserName}")]
+        public async Task<IActionResult> CreateOrder(string UserName)
+        {
+            var order = await _OrderService.CreateOrder(UserName,);
+            return Ok(order);
+        }
+    }
+}
