2133f614023594b234ccdfe8d27a882a97220ec7
Infarh <shmachilin@yandex.ru>
2021-10-14 20:09:44 +0300

    Клиент для WebAPI управления сотрдуниками

---
+++ Services/WebStore.WebAPI.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using WebStore.Domain.Models;
+using WebStore.Interfaces.Services;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Employees
+{
+    public class EmployeesClient : BaseClient, IEmployeesData
+    {
+        public EmployeesClient(HttpClient Client) : base(Client, "api/employees") { }
+
+
+        public IEnumerable<Employee> GetAll()
+        {
+            var employees = Get<IEnumerable<Employee>>(Address);
+            return employees;
+        }
+
+        public Employee GetById(int id)
+        {
+            var result = Get<Employee>($"{Address}/{id}");
+            return result;
+        }
+
+        public int Add(Employee employee)
+        {
+            var response = Post(Address, employee);
+            var added_employee = response.Content.ReadFromJsonAsync<Employee>();
+            var id = added_employee.Id;
+            return id;
+        }
+
+        public void Update(Employee employee)
+        {
+            Put(Address, employee);
+        }
+
+        public bool Delete(int id)
+        {
+            var response = Delete($"{Address}/{id}");
+            var success = response.IsSuccessStatusCode;
+            return success;
+        }
+    }
+}
