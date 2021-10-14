ef4edcfec4271b11df068f38361f02386c1052f7
Infarh <shmachilin@yandex.ru>
2021-10-14 19:37:17 +0300

    Подключение БД к проекту WebAPI

--- Services/WebStore.WebAPI/Startup.cs
+++ Services/WebStore.WebAPI/Startup.cs
@@ -1,9 +1,12 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using WebStore.DAL.Context;
 
 namespace WebStore.WebAPI
 {
@@ -11,6 +14,28 @@ namespace WebStore.WebAPI
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            var database_type = Configuration["Database"];
+
+            switch (database_type)
+            {
+                default: throw new InvalidOperationException($"Тип БД {database_type} не поддерживается");
+
+                case "SqlServer":
+                    services.AddDbContext<WebStoreDB>(opt =>
+                        opt.UseSqlServer(Configuration.GetConnectionString(database_type)));
+                    break;
+
+                case "Sqlite":
+                    services.AddDbContext<WebStoreDB>(opt =>
+                        opt.UseSqlite(Configuration.GetConnectionString(database_type),
+                            o => o.MigrationsAssembly("WebStore.DAL.Sqlite")));
+                    break;
+
+                //case "InMemory":
+                //    services.AddDbContext<WebStoreDB>(opt => opt.UseInMemoryDatabase("WebStore.db"));
+                //    break;
+            }
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
