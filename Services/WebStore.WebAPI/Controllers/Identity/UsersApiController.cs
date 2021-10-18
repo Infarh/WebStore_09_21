40263a48047c2b8e5e65f54ff52425046cba13ec
Infarh <shmachilin@yandex.ru>
2021-10-18 19:39:47 +0300

    Пустые заготовки контроллеров для Identity

---
+++ Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.WebAPI.Controllers.Identity
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersApiController : ControllerBase
+    {
+    }
+}
