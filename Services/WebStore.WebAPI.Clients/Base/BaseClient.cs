45e82f4869d752cb853806815e8fc9f44aeeaf68
Infarh <shmachilin@yandex.ru>
2021-10-18 20:25:15 +0300

    Доработка класса Базового клиента

--- Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
+++ Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
@@ -1,10 +1,11 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace WebStore.WebAPI.Clients.Base
 {
-    public abstract class BaseClient
+    public abstract class BaseClient : IDisposable
     {
         protected HttpClient Http { get; }
 
@@ -46,6 +47,32 @@ namespace WebStore.WebAPI.Clients.Base
         {
             var response = await Http.DeleteAsync(url).ConfigureAwait(false);
             return response;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            //GC.SuppressFinalize(this);
+        }
+
+        //~BaseClient()
+        //{
+        //    Dispose(false);
+        //}
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if(_Disposed) return;
+            _Disposed = true;
+
+            if (disposing)
+            {
+                // должны освободить управляемые ресурсы
+                //Http.Dispose(); // не мы создали, не нам и освобождать!
+            }
+
+            // должны освободить неуправляемые ресурсы
         }
     }
 }
