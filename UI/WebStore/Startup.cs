d17a91bbe83f68aa684693bfcf97765fc64e0bce
Infarh <shmachilin@yandex.ru>
2021-10-18 20:45:38 +0300

    Регистрация клиентов WebAPI в основном приложении

--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -18,6 +18,7 @@ using WebStore.Interfaces.TestAPI;
 using WebStore.Services.Data;
 using WebStore.Services.Services.InCookies;
 using WebStore.WebAPI.Clients.Employees;
+using WebStore.WebAPI.Clients.Identity;
 using WebStore.WebAPI.Clients.Orders;
 using WebStore.WebAPI.Clients.Products;
 using WebStore.WebAPI.Clients.Values;
@@ -51,8 +52,20 @@ namespace WebStore
             }
 
             services.AddIdentity<User, Role>()
-               .AddEntityFrameworkStores<WebStoreDB>()
+               //.AddEntityFrameworkStores<WebStoreDB>()
                .AddDefaultTokenProviders();
+
+            services.AddHttpClient("WebStoreWebAPIIdentity", client => client.BaseAddress = new(Configuration["WebAPI"]))
+               .AddTypedClient<IUserStore<User>, UsersClient>()
+               .AddTypedClient<IUserRoleStore<User>, UsersClient>()
+               .AddTypedClient<IUserPasswordStore<User>, UsersClient>()
+               .AddTypedClient<IUserEmailStore<User>, UsersClient>()
+               .AddTypedClient<IUserPhoneNumberStore<User>, UsersClient>()
+               .AddTypedClient<IUserTwoFactorStore<User>, UsersClient>()
+               .AddTypedClient<IUserClaimStore<User>, UsersClient>()
+               .AddTypedClient<IUserLoginStore<User>, UsersClient>()
+               .AddTypedClient<IRoleStore<Role>, RolesClient>()
+                ;
 
             services.Configure<IdentityOptions>(opt =>
             {
