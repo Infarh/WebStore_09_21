027cb835f196865739c42b34dd137b7b1f885510
Infarh <shmachilin@yandex.ru>
2021-10-21 20:00:36 +0300

    Жёсткая настрока системы конфигурации в коде

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -2,6 +2,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using WebStore.Services.Data;
 
 namespace WebStore
@@ -20,6 +22,14 @@ namespace WebStore
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(host => host
                    .UseStartup<Startup>()
+                   //.ConfigureLogging((host, log) => log
+                   //    .ClearProviders()
+                   //    .AddDebug()
+                   //    .AddConsole(c => c.IncludeScopes = true)
+                   //    .AddEventLog()
+                   //    .AddFilter("Microsoft", LogLevel.Information)
+                   //    .AddFilter<ConsoleLoggerProvider>("Microsoft.EntityFrameworkCore", LogLevel.Warning)
+                   // )
                 )
             ;
     }
