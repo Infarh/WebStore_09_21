93fc187edf96ab779d8ed07a48f60cd345e96d96
Infarh <shmachilin@yandex.ru>
2021-10-25 21:13:58 +0300

    Подготовка к тестированию сервиса корзины

--- Tests/WebStore.Services.Tests/Services/CartServiceTests.cs
+++ Tests/WebStore.Services.Tests/Services/CartServiceTests.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 using WebStore.Domain.ViewModels;
+using WebStore.Interfaces.Services;
+using WebStore.Services.Services;
 using Assert = Xunit.Assert;
 
 namespace WebStore.Services.Tests.Services
@@ -11,6 +15,11 @@ namespace WebStore.Services.Tests.Services
     public class CartServiceTests
     {
         private Cart _Cart;
+
+        private Mock<IProductData> _ProductDataMock;
+        private Mock<ICartStore> _CartStoreMock;
+
+        private ICartService _CartService;
 
         [TestInitialize]
         public void TestInitialize()
@@ -23,6 +32,51 @@ namespace WebStore.Services.Tests.Services
                     new() { ProductId = 2, Quantity = 3 },
                 }
             };
+
+            _ProductDataMock = new Mock<IProductData>();
+            _ProductDataMock
+               .Setup(c => c.GetProducts(It.IsAny<ProductFilter>()))
+               .Returns(new[]
+                {
+                    new Product
+                    {
+                        Id = 1,
+                        Name = "Product 1",
+                        Price = 1.1m,
+                        Order = 1,
+                        ImageUrl = "img_1.png",
+                        Brand = new Brand { Id = 1, Name = "Brand 1", Order = 1},
+                        SectionId = 1,
+                        Section = new Section{ Id = 1, Name = "Section 1", Order = 1 },
+                    },
+                    new Product
+                    {
+                        Id = 2,
+                        Name = "Product 2",
+                        Price = 2.2m,
+                        Order = 2,
+                        ImageUrl = "img_2.png",
+                        Brand = new Brand { Id = 2, Name = "Brand 2", Order = 2},
+                        SectionId = 2,
+                        Section = new Section{ Id = 2, Name = "Section 2", Order = 2 },
+                    },
+                    new Product
+                    {
+                        Id = 3,
+                        Name = "Product 3",
+                        Price = 3.3m,
+                        Order = 3,
+                        ImageUrl = "img_3.png",
+                        Brand = new Brand { Id = 3, Name = "Brand 3", Order = 3},
+                        SectionId = 3,
+                        Section = new Section{ Id = 3, Name = "Section 3", Order = 3 },
+                    },
+                });
+
+            _CartStoreMock = new Mock<ICartStore>();
+            _CartStoreMock.Setup(c => c.Cart).Returns(_Cart);
+
+            _CartService = new CartService(_CartStoreMock.Object, _ProductDataMock.Object);
         }
 
         [TestMethod]
