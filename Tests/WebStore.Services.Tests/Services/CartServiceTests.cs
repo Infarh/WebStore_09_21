82096be2c239b4d8d0d79f080bcce057513b6fcc
Infarh <shmachilin@yandex.ru>
2021-10-25 21:05:50 +0300

    Тестирование моделей, используемых в корзине

---
+++ Tests/WebStore.Services.Tests/Services/CartServiceTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebStore.Domain.Entities;
+using WebStore.Domain.ViewModels;
+using Assert = Xunit.Assert;
+
+namespace WebStore.Services.Tests.Services
+{
+    [TestClass]
+    public class CartServiceTests
+    {
+        private Cart _Cart;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _Cart = new Cart
+            {
+                Items = new List<CartItem>
+                {
+                    new() { ProductId = 1, Quantity = 1 },
+                    new() { ProductId = 2, Quantity = 3 },
+                }
+            };
+        }
+
+        [TestMethod]
+        public void Cart_Class_ItemsCount_returns_Correct_Quantity()
+        {
+            var cart = _Cart;
+
+            var expected_items_count = cart.Items.Sum(i => i.Quantity);
+
+            var actual_items_count = cart.ItemsCount;
+
+            Assert.Equal(expected_items_count, actual_items_count);
+        }
+
+        [TestMethod]
+        public void CartViewModel_Returns_Correct_ItemsCount()
+        {
+            var cart_view_model = new CartViewModel
+            {
+                Items = new[]
+                {
+                    ( new ProductViewModel { Id = 1, Name = "Product 1", Price = 0.5m }, 1 ),
+                    ( new ProductViewModel { Id = 2, Name = "Product 2", Price = 1.5m }, 3 ),
+                }
+            };
+
+            var expected_items_count = cart_view_model.Items.Sum(i => i.Quantity);
+            
+            var actual_items_count = cart_view_model.ItemsCount;
+
+            Assert.Equal(expected_items_count, actual_items_count);
+        }
+
+        [TestMethod]
+        public void CartViewModel_Returns_Correct_TotalPrice()
+        {
+            var cart_view_model = new CartViewModel
+            {
+                Items = new[]
+                {
+                    ( new ProductViewModel { Id = 1, Name = "Product 1", Price = 0.5m }, 1 ),
+                    ( new ProductViewModel { Id = 2, Name = "Product 2", Price = 1.5m }, 3 ),
+                }
+            };
+
+            var expected_total_price = cart_view_model.Items.Sum(item => item.Quantity * item.Product.Price);
+
+            var actual_total_price = cart_view_model.TotalPrice;
+
+            Assert.Equal(expected_total_price, actual_total_price);
+        }
+    }
+}
