d36747b1744ceb7a1b4c539cc5c5fa6b4d21366f
Infarh <shmachilin@yandex.ru>
2021-10-28 20:09:26 +0300

    Основная логика таг-хелпера

--- UI/WebStore/TagHelpers/ActiveRoute.cs
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace WebStore.TagHelpers
 {
@@ -10,6 +12,9 @@ namespace WebStore.TagHelpers
     public class ActiveRoute : TagHelper
     {
         private const string AttributeName = "ws-is-active-route";
+
+        [HtmlAttributeName("ws-is-active-route-active")]
+        public string ActiveCssClass { get; set; } = "active";
 
         [HtmlAttributeName("asp-controller")]
         public string Controller { get; set; }
@@ -28,8 +33,47 @@ namespace WebStore.TagHelpers
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            if (IsActive())
+                MakeActive(output);
 
             output.Attributes.RemoveAll(AttributeName);
+        }
+
+        private bool IsActive()
+        {
+            var route_values = ViewContext.RouteData.Values;
+
+            var route_controller = route_values["controller"]?.ToString();
+            var route_action = route_values["action"]?.ToString();
+
+            //if (!string.IsNullOrEmpty(Action) && !string.Equals(Action, route_action))
+            //    return false;
+            if (Action is { Length: > 0 } action && !string.Equals(action, route_action))
+                return false;
+
+            if (Controller is { Length: > 0 } controller && !string.Equals(controller, route_controller))
+                return false;
+
+            foreach (var (key, value) in RouteValues)
+                if (!route_values.ContainsKey(key) || route_values[key]?.ToString() != value)
+                    return false;
+
+            return true;
+        }
+
+        private void MakeActive(TagHelperOutput output)
+        {
+            var class_attribute = output.Attributes.FirstOrDefault(attr => attr.Name == "class");
+
+            if(class_attribute is null)
+                output.Attributes.Add("class", ActiveCssClass);
+            else
+            {
+                if(class_attribute.Value?.ToString()?.Contains(ActiveCssClass) ?? false)
+                    return;
+
+                output.Attributes.SetAttribute("class", $"{class_attribute.Value} {ActiveCssClass}");
+            }
         }
     }
 }
