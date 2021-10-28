1d7e20ac73a3235e5e29f745a4a59a45a3e9101a
Infarh <shmachilin@yandex.ru>
2021-10-28 20:34:39 +0300

    Выделение активной секции

---
+++ UI/WebStore/ViewModels/SelectableSectionsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using WebStore.Domain.ViewModels;
+
+namespace WebStore.ViewModels
+{
+    public class SelectableSectionsViewModel
+    {
+        public IEnumerable<SectionViewModel> Sections { get; set; }
+
+        public int? SectionId { get; set; }
+
+        public int? ParentSectionId { get; set; }
+    }
+}
