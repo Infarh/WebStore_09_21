16c189d48349fa517dce19d715064be73448f420
Infarh <shmachilin@yandex.ru>
2021-11-01 21:22:39 +0300

    Таг-хелпер переключалки страниц

---
+++ Common/WebStore.Domain/ViewModels/PageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebStore.Domain.ViewModels
+{
+    public class PageViewModel
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+
+        public int TotalItems { get; set; }
+
+        public int TotalPages => PageSize == 0 ? 0 : (int)Math.Ceiling((double)TotalItems / PageSize);
+    }
+}
--- UI/WebStore/Controllers/CatalogController.cs
+++ UI/WebStore/Controllers/CatalogController.cs
@@ -38,7 +38,13 @@ namespace WebStore.Controllers
             {
                 BrandId = BrandId,
                 SectionId = SectionId,
-                Products = products.OrderBy(p => p.Order).ToView()
+                Products = products.OrderBy(p => p.Order).ToView(),
+                PageViewModel = new()
+                {
+                    Page = Page,
+                    PageSize = page_size ?? 0,
+                    TotalItems = total_count,
+                }
             };
 
             return View(view_model);
