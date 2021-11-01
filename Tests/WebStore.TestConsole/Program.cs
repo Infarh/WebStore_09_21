7cc3fa744d7ced673ae899aaa6db59879f173d45
Infarh <shmachilin@yandex.ru>
2021-11-01 19:45:41 +0300

    Пример автоматической генерации клиентов для WebAPI

--- Tests/WebStore.TestConsole/Program.cs
+++ Tests/WebStore.TestConsole/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace WebStore.TestConsole
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Clients;
+
+namespace WebStore.TestConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-      
+            var client = new HttpClient()
+            {
+                //BaseAddress = new Uri("http://localhost:5001")
+            };
+
+            //var api = new WebAPIClient("http://localhost:5001", client);
+
+            //var employee = await api.Employees4Async(2);
         }
     }
 }
