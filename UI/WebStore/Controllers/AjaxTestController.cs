0857ad69c25b430f74f9f66f0fdb506d4d63142c
Infarh <shmachilin@yandex.ru>
2021-11-04 20:27:05 +0300

    Заготовка

---
+++ UI/WebStore/Controllers/AjaxTestController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class AjaxTestController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
