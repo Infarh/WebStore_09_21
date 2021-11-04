a008983c28dda048a7fc08921bbb98276d7ea6c9
Infarh <shmachilin@yandex.ru>
2021-11-04 21:04:46 +0300

    Управление консолью на этапе отладки с помощью ненавязчивого AJAX

---
+++ UI/WebStore/Controllers/API/ConsoleController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers.API
+{
+    [ApiController, Route("api/[controller]")]
+    public class ConsoleController : ControllerBase
+    {
+        [HttpGet("clear")]
+        public void Clear() => Console.Clear();
+
+        [HttpGet("WriteLine")]
+        public void WriteLine(string Message) => Console.WriteLine(Message);
+    }
+}
