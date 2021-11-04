4eee7b01442dd05e6322af90f8a84d2215052ef1
Infarh <shmachilin@yandex.ru>
2021-11-04 20:37:48 +0300

    Подготовка источника данных для скрипта - WebAPI

---
+++ UI/WebStore/ViewModels/AjaxTestDataViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public class AjaxTestDataViewModel
+    {
+        public int Id { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime ServerTime { get; set; } = DateTime.Now;
+    }
+}
