43b9ad3477569a5d6c1e0f162ba1ea0d282f62be
Infarh <shmachilin@yandex.ru>
2021-11-04 22:59:08 +0300

    Загрузка данных в blazor с сервера

---
+++ UI/WebStore.BlazorUI/Models/ProductInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace WebStore.BlazorUI.Models
+{
+    public class ProductInfo
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Image { get; set; }
+    }
+}
---
+++ UI/WebStore/Controllers/API/ProductsApiController.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Controllers.API
+{
+    [ApiController, Route("api/products")]
+    public class ProductsApiController : ControllerBase
+    {
+        private readonly IProductData _ProductData;
+
+        public ProductsApiController(IProductData ProductData) => _ProductData = ProductData;
+
+        private class ProductInfo
+        {
+            public int Id { get; set; }
+
+            public string Title { get; set; }
+
+            public decimal Price { get; set; }
+
+            public string Image { get; set; }
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var products = _ProductData.GetProducts();
+
+            var infos = products.Products.Select(p => new ProductInfo
+            {
+                Id = p.Id,
+                Title = p.Name,
+                Price = p.Price,
+                Image = p.ImageUrl
+            });
+
+            return Ok(infos);
+        }
+    }
+}
